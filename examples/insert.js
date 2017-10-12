a7ec4fb02af5e8dc3551d36c9d899ab40ec31238
JacopoDaeli <jacopo.daeli@gmail.com>
2017-10-11 17:23:01 -0700

    Update stuff

--- examples/insert.js
+++ examples/insert.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// NOTE: https://github.com/JacopoDaeli/regit-db-example/tree/foo
+
 const RegitClient = require('..').RegitClient
 
 const client = new RegitClient()
@@ -7,14 +9,23 @@ const client = new RegitClient()
 client.connect({
   token: 'token',
   username: 'username',
-  repo: 'repo'
+  repo: 'regit-db-example'
 }, (err, db) => {
   if (err) return console.error(err)
 
-  const collection = db.collection('foo')
-  collection.insertOne({
-    beep: 'boop',
+  const foo = db.collection('foo')
+  foo.insertOne({
+    foo: 'bar',
     isCool: true
+  }, (err, item) => {
+    if (err) console.error(err)
+    else console.log(item)
+  })
+
+  const beep = db.collection('beep')
+  beep.insertOne({
+    beep: 'boop',
+    isAwesome: true
   }, (err, item) => {
     if (err) console.error(err)
     else console.log(item)
