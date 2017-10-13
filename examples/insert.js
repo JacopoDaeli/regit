c4061ac8645e5f8e26181bd6928405db90d0646b
JacopoDaeli <jacopo.daeli@gmail.com>
2017-10-13 11:07:35 -0700

    Serialize inserts

--- examples/insert.js
+++ examples/insert.js
@@ -8,6 +8,7 @@ client.connect(config, (err, db) => {
   if (err) return console.error(err)
 
   console.log(`Connected to db ${config.repo}.`)
+  console.log(`Current sha ${db.sha}.`)
 
   const foo = db.collection('foo')
   foo.insertOne({
@@ -26,12 +27,21 @@ client.connect(config, (err, db) => {
     else console.log(item, id)
   })
 
-  const beep = db.collection('beep')
-  beep.insertOne({
-    beep: 'boop',
-    isAwesome: true
+  foo.insertOne({
+    foo: 'bar2',
+    isCool: false
   }, (err, item, id) => {
     if (err) console.error(err)
     else console.log(item, id)
   })
+
+
+  // const beep = db.collection('beep')
+  // beep.insertOne({
+  //   beep: 'boop',
+  //   isAwesome: true
+  // }, (err, item, id) => {
+  //   if (err) console.error(err)
+  //   else console.log(item, id)
+  // })
 })
