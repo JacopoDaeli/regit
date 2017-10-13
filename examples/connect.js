d608cc2ad674b1863e466cfb75fe2d118ae210ae
JacopoDaeli <jacopo.daeli@gmail.com>
2017-10-13 12:52:11 -0700

    Update examples

--- examples/connect.js
+++ examples/connect.js
@@ -7,14 +7,7 @@ const client = new RegitClient()
 client.connect(config, (err, db) => {
   if (err) return console.error(err)
 
+  // Print some database info
   console.log(`Connected to db ${config.repo}.`)
   console.log(`Current sha ${db.sha}.`)
-
-  const repo = db.ghrepo
-  const number = 10
-  const sha = 'fa4a04899451e2edf91813536ba152bc216c6b88'
-  repo.squash(number, sha, (err) => {
-    if (err) console.error(err)
-    else console.log(`Squashed PR ${number}.`)
-  })
 })
--- examples/insert.js
+++ examples/insert.js
@@ -7,10 +7,14 @@ const client = new RegitClient()
 client.connect(config, (err, db) => {
   if (err) return console.error(err)
 
+  // Print some database info
   console.log(`Connected to db ${config.repo}.`)
   console.log(`Current sha ${db.sha}.`)
 
+  // Create foo collection
   const foo = db.collection('foo')
+
+  // Insert element in foo collection
   foo.insertOne({
     foo: 'bar',
     isCool: true
@@ -19,6 +23,7 @@ client.connect(config, (err, db) => {
     else console.log(item, id)
   })
 
+  // Insert another element in foo collection
   foo.insertOne({
     foo: 'bar',
     isCool: false
@@ -27,14 +32,7 @@ client.connect(config, (err, db) => {
     else console.log(item, id)
   })
 
-  foo.insertOne({
-    foo: 'bar2',
-    isCool: false
-  }, (err, item, id) => {
-    if (err) console.error(err)
-    else console.log(item, id)
-  })
-
+  // Create beep collection and insert an element
   const beep = db.collection('beep')
   beep.insertOne({
     beep: 'boop',
